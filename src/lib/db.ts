7a9bdce54231f7aa41b1a57b4d6d3a6fe358d96d
piyush0049 <piyushjoshi81204@gmail.com>
2025-10-12 00:49:27 +0530

    db name change

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -21,7 +21,7 @@ if (!cached) cached = global.mongooseCache = { conn: null, promise: null };
 export async function connectToDatabase() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, { dbName: process.env.MONGODB_DB || "aerosafe" });
+    cached.promise = mongoose.connect(MONGODB_URI, { dbName: process.env.MONGODB_DB || "Aerosafe" });
   }
   cached.conn = await cached.promise;
   return cached.conn;
